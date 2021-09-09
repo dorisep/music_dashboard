983897ec2c52a16f8c20768c9e60940ce5a51266
dorisep <ed.doris@gmail.com>
2021-09-09 13:14:51 -0500

    adding feature to flask
    trying to serve the data a json to page.

--- data/app.py
+++ data/app.py
@@ -1,7 +1,7 @@
 
 from flask import g, Flask, app, render_template, redirect
 import sqlite3
-import os
+import json
 
 # assign an instance of Flask
 app = Flask(__name__)
@@ -12,27 +12,29 @@ def get_db():
     db = getattr(g, '_music_dashboard', None) 
     if db is None:
         db= g._music_dahboard = sqlite3.connect(DATABASE)
-        db.row_factory = sqlite3.Row       
+        # db.row_factory = sqlite3.Row       
     return db
 # set query with in a flask context for db
 def query_db(query, args=(), one=False):
     with app.app_context():
         cur = get_db().execute(query, args)
         rv = cur.fetchall()
-        cur.close()
-    return (rv[0] if rv else None) if one else rv
+    return json.dumps(rv)
+    #     cur.close()
+    # return (rv[0] if rv else None) if one else rv
 # create teardown for db connection
 @app.teardown_appcontext
 def close_connection (exception):
     db = getattr(g, 'music_dashbaord', None)
     if db is not None:
         db.close()
-# create index route
+# create index route and query db then iterate over data
 @app.route('/')
 def index():
     data = query_db('select * from albums where week_num = 20')
-    for k in data:
-        print(k['album'])
+
+    # for k in data:
+    #     print(k['album'])
     return render_template('index.html', data = data)
 # run app in debug mode for developement 
 if __name__ == "__main__":
